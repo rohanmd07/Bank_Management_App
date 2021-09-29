4c8e1ec5f4aa57b7d0f1c2e8d2939e9a93ed1800
Rohan Negi <rohan.negi@ad.fc.net>
2021-09-29 12:40:40 +0530

    Forth commit

--- src/com/mybank/services/BankApp.java
+++ src/com/mybank/services/BankApp.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class BankApp {
 
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Dear customer, Welcome to our e-banking facility");
 		System.out.println("Please choose the type of service you want");
